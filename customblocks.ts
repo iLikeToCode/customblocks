053338bd4d1dc9b7c5ac78fc572618adcb357a58
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2024-05-10 17:15:01 +0100

    Update pxt.json, main.blocks, customblocks.ts, test.ts

---
+++ customblocks.ts
@@ -0,0 +1,6 @@
+//% color="#85d8ff" weight=120 icon="\uf091"
+//% groups=['Edit Guide','Examples']
+//% block="Custom"
+namespace custom {
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,23 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+//% color="#85d8ff" weight=120 icon="\uf091"
+//% groups=['Edit Guide','Examples']
+//% block="Custom"
+namespace custom {
+    //% blockId=custom_1
+    //% block="Javascript -> Explorer -> guide.ts"
+    //% group="Guide"
+    export function guide() { };
+
+    /**
+     * Display text on the display, one character at a time. If the string fits on the screen (i.e. is one letter), does not scroll.
+     * @param text the text to scroll on the screen, eg: "Hello!"
+     */
+    //% blockId=custom_2
+    //% block="Show Text $text"
+    //% group="Examples"
+    export function showText(text: string = "Hello") {
+        basic.showString(text)
+    };
+}
+
+// This file is deleted when imported as extension
+// Meant to be used as a guide only
